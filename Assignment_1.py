21d75ecafe07437f9718d43defbf9a2e1d3cbee7
Arghya Bhattacharjee <143819154+Arghya876@users.noreply.github.com>
2023-09-01 20:03:32 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+
+print(a/b) #Division
+
+# Q1. Floor Division
+print(a//b)  
+
+# Q2. Power of a and b 
+print(a**b)
+
+# Q3. Check the variables equal or not
+if a==b:
+  print("True")
+else:  
+  print("False")
